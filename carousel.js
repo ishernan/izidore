730bc294d3da65af223ce7d3d3ce37938ea3c43f
Isaac Hernandez <isaac.hernandez@outlook.com>
2021-05-31 16:06:11 +0200

    carousel jquery

---
+++ carousel.js
@@ -0,0 +1,36 @@
+
+// $(document).ready(function () {
+//   $(".owl-carousel").owlCarousel(); 
+//   });
+
+
+$(".owl-carousel").owlCarousel({
+  autoplay: false, 
+  autoplayhoverpause: true,
+  autoplaytimeout: 100,
+  items: 4,
+  nav: true, 
+  loop:true,
+  responsive:{
+    0: {
+      items: 1,
+      dots: false
+    },
+    485: {
+      items : 2,
+      dots : false
+    },
+    728: {
+      items: 3,
+      dots: false
+    }, 
+    960: {
+      items : 4,
+      dots : false
+    },
+    1200 : {
+      items : 5,
+      dots: true
+    }
+  }
+}); 
