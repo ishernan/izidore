10adf130b25045a39f465c35c59f810c3827acb3
Isaac Hernandez <isaac.hernandez@outlook.com>
2021-06-04 12:08:16 +0200

    formulaire

--- rating.js
+++ rating.js
@@ -1,18 +1,18 @@
 //etoiles
 
-var evalIndex =-1;   //quand il y a pas evaluation
+var evalIndex =-1;   //quand il y a pas d'evaluation
 
 $(document).ready(function() {
   couleurDefault();  
   
   if(localStorage.getItem('evalIndex') !=null){
     setEtoile(parseInt(localStorage.getItem('evalIndex'))); 
-
   }
 
   $('.bi-star-fill').on('click', function () { 
     evalIndex= parseInt($(this).attr('data-index')); 
     localStorage.setItem('evalIndex', evalIndex); 
+    $('#note').text(evalIndex+1); 
    });
    
   $('.bi-star-fill').on('mouseover', function() { 
@@ -38,9 +38,43 @@ function setEtoile(max) {
 }
 
 function couleurDefault(){
-  $('.bi-star-fill').css('color', 'rgb(143, 140, 140)');
+  $('.bi-star-fill').css('color','#A9A9A9' ); //'#58B58D'
 }
 
 
 
-  // do something...
+//formulaire 
+
+const formulaire  = document.querySelector('#formulaire');
+const btnEnvoyer  = document.querySelector('#btnEnvoyer');
+const btnLoad     = document.querySelector('#btnLoad');
+const toast       = document.querySelector('.toast');
+
+
+formulaire.addEventListener('submit', (e)=> {
+  e.preventDefault();
+
+  const data = new FormData(formulaire);
+
+  console.log('user :', data.get('user'))
+  console.log('password :', data.get('passe'))
+  console.log('Avis :', data.get('avis'))
+  console.log(evalIndex+1)
+
+
+  btnEnvoyer.classList.add("d-none")
+  btnLoad.classList.remove('d-none')
+
+  setTimeout(()=> {
+   btnEnvoyer.classList.remove("d-none")
+   btnLoad.classList.add('d-none')
+
+   const toastEvent = new bootstrap.Toast(toast)
+   toastEvent.show()
+
+  }, 2000); 
+
+  formulaire.reset();//pour remettre a zero le formulaire  
+  
+  })
+  
